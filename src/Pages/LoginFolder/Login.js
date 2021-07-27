37f155680bf5436329d78dd09d025ec010ed5086
unknown <gustavogst01@gmail.com>
2021-07-27 08:03:06 -0300

    Reset basico de margin e padding criado na pasta public (index.html), estilização basica da pagina de login, form basico e ao clicar nos letreiros de cadastro, redireciona para pagina de cadastro

--- src/Pages/LoginFolder/Login.js
+++ src/Pages/LoginFolder/Login.js
@@ -1,11 +1,106 @@
 import React from 'react';
+import {ScreenContainer, InputContainer, Title, Img} from './styled'
+import logo from '../../assets/logo.png'
+import useForm from '../../hooks/useForm'
+import { styled } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom';
+import {goToCadastroSignUp} from "../../routes/cordinator"
 
 
-function Login () {
+const ButtonSubmit = styled(Button)({
+ marginTop: 15,
+ height: 45
+})
+
+const ButtonSignUp= styled(Button)({
+  textTransform: 'none'
+ })
+ 
+
+const Login = () => {
+  const [form, onChange, clear] = useForm({email: "", password: ""})
+
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+
+    console.log(form)
+
+  }
+
+  const history = useHistory()
+
   return (
-    <>
-      <h1>OLL</h1>
-    </>
+    <ScreenContainer>
+      <Img src={logo}></Img>
+
+      <Title>
+        <p>Entrar</p>
+      </Title>
+
+      <InputContainer>
+         <form onSubmit={onSubmitForm}>
+
+         <TextField
+          required
+          label="E-mail"
+          placeholder="email@email.com"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          fullWidth
+          type={"email"}
+
+          name={"email"}
+          value={form.email}
+          onChange={onChange}
+        />
+
+        <TextField
+          required
+          label="Senha"
+          placeholder="Minimo 6 caracteres"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          fullWidth
+          type={"password"}
+
+          name={"password"}
+          value={form.password}
+          onChange={onChange}
+        />
+
+        <ButtonSubmit 
+        type={"submit"}
+        fullWidth
+        variant={"contained"}
+        color={"primary"}
+        >
+          Entrar
+        </ButtonSubmit>    
+     
+         </form>
+      </InputContainer>
+
+      <div className="SignUpButtonContainer">
+       <ButtonSignUp 
+        onClick={() => goToCadastroSignUp(history)}
+        type={"submit"}
+        fullWidth
+        variant={"text"}
+        
+        >
+        Não possui cadastro? Clique aqui.
+        </ButtonSignUp>    
+      </div>
+
+    </ScreenContainer>
   );
 }
 
---
+++ src/Pages/LoginFolder/styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const ScreenContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100vw;
+`
+
+export const InputContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 80vw;
+max-width: 450px;
+align-items: center;
+margin-bottom: 20px;
+`
+export const Title = styled.p`
+font-size: 16px;
+font-family: Roboto;
+letter-spacing: -0.39px;
+`
+
+export const Img = styled.img`
+width: 100px;
+height: 58px;
+margin: 68px 128px 16px;
+object-fit: contain;
+`
